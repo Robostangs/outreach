525b132cec4a10439e0e4ffd9dfa2710db1683b1
esoterik <nightskyla@gmail.com>
2013-06-22 22:18:11 -0400

    no longer redirect admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_user!
+  before_filter :check_user
+
+  private 
+  def check_user 
+    authenticate_user! unless request.fullpath.include? "/admin"
+  end
+
 end
