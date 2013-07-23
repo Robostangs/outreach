d56fdd43680b6da6b8dd7c96ad41f59868e2abc7
esoterik <nightskyla@gmail.com>
2013-07-23 13:52:55 -0400

    removed duplicate :email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :email, :first_name, :last_name
+  attr_accessible :first_name, :last_name
 
   has_many :signups
   has_many :events, :through => :signups
