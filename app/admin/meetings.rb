cb885492dbf21618921a9b6d928d45e6773ca57c
sky <nightskyla@gmail.com>
2013-09-07 13:02:08 -0400

    display attendance uploading

--- app/admin/meetings.rb
+++ app/admin/meetings.rb
@@ -1,16 +1,17 @@
 ActiveAdmin.register Meeting do
+	config.clear_action_items!
+
 	action_item :only => :index do
 		link_to 'Upload Attendance', :action => 'upload_attend'
 	end
 
 	collection_action :upload_attend do
 		# create app/views/admin/meetings/upload_attend.html.erb
-		render "admin/csv/upload_attend"
-		redirect_to :action => :index, :notice => "Upload successful." 
+		render "admin/meetings/upload_attend"
 	end
 
-	collection_action :import_attend, :method => :post do
+	collection_action :import_attend do
 		CsvDb.import_meeting("meeting", params[:dump][:file])
-		redirect_to :action => :index, :notice => "Import successful." 
+		redirect_to :action => :index, :notice => "Upload successful." 
 	end
 end
