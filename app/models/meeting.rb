416d134d3e4b986057f9a0687c875d02be536959
sky <nightskyla@gmail.com>
2013-09-07 11:44:12 -0400

    meetings pagination

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,10 @@
 class Meeting < ActiveRecord::Base
   attr_accessible :description, :mandatory, :meeting_date
 
+	scope :desc, order("meetings.created_at DESC")
+
+	self.per_page = 10
+
 	has_many :attendances
 	has_many :users, :through => :attendances
 end
