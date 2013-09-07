f6ded1780ce6b3c4664ebe1064aad049bc97c839
sky <nightskyla@gmail.com>
2013-09-07 11:42:33 -0400

    pagination for meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,13 @@
-class MeetingsController < InheritedResources::Base
+class MeetingsController < ApplicationController
+	def index
+		@meetings = Meeting.desc.paginate(:page => params[:page])
+	end
+  def show
+    @meeting = Meeting.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @meeting }
+    end
+  end
 end
